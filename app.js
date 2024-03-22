8b461a931f5a1ab79223025bc08350236530258f
Moses <Moseschizaram8@gmail.com>
2024-03-22 22:27:26 +0100

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3500;
+
+app.use(expres.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Database connection
+// mongoose.connect("mongodb://localhost:27017/Trippa", {
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+// if (db) console.log("Database connection successful");
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Trippa");
