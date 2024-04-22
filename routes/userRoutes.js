9f201dc8758b4a4958312ecefcc3d832c52f6c87
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2024-04-22 09:54:42 +0100

    fixing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 // routes/userRoutes.js
 const express = require("express");
 const router = express.Router();
-const { userController } = require("../controllers/userController");
+const { getUserProfile, updateUserProfile, getAllUsers, getUserById, deleteUser } = require("../controllers/userController");
 
 // CRUD endpoints for users
-router.route("/profile").get(userController.getUserProfile).put(userController.updateUserProfile);
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
 // router.post("/", userController.createUser);
 //router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
